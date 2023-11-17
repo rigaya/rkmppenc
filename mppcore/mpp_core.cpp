8c410c4d89b4eda62aed7fca99cec92bb49f4053
rigaya <rigaya34589@live.jp>
2023-11-17 21:58:27 +0900

    --vpp-yadif/--vpp-nnediでbob化する際、--vpp-rffなしでrffな動画を処理するとtimestamp周りの計算がおかしくなりエラー終了する問題を修正。

--- mppcore/mpp_core.cpp
+++ mppcore/mpp_core.cpp
@@ -1404,6 +1404,7 @@ RGY_ERR MPPCore::AddFilterOpenCL(std::vector<std::unique_ptr<RGYFilter>>&clfilte
         param->frameIn = inputFrame;
         param->frameOut = inputFrame;
         param->baseFps = m_encFps;
+        param->timebase = m_outputTimebase;
         param->bOutOverwrite = false;
         auto sts = filter->init(param, m_pLog);
         if (sts != RGY_ERR_NONE) {
