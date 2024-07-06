6fd4760ea6571e2022c300a31ea1c1cb98fab6bd
rigaya <rigaya34589@live.jp>
2024-07-06 13:11:13 +0900

    --vpp-tweakにチャネルごとの制御を追加。

--- mppcore/mpp_core.cpp
+++ mppcore/mpp_core.cpp
@@ -1875,6 +1875,7 @@ RGY_ERR MPPCore::AddFilterOpenCL(std::vector<std::unique_ptr<RGYFilter>>&clfilte
         param->tweak = inputParam->vpp.tweak;
         param->frameIn = inputFrame;
         param->frameOut = inputFrame;
+        param->vui = vuiInfo;
         param->baseFps = m_encFps;
         param->bOutOverwrite = true;
         auto sts = filter->init(param, m_pLog);
