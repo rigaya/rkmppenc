e49c60fcf7b3afb956aa9af29d0970f97aa8d16b
rigaya <rigaya34589@live.jp>
2025-08-23 11:22:25 +0900

    他のエンコーダの更新を反映。

--- mppcore/rgy_bitstream_aac.h
+++ mppcore/rgy_bitstream_aac.h
@@ -3,7 +3,7 @@
 // -----------------------------------------------------------------------------------------
 // The MIT License
 //
-// Copyright (c) 2019 rigaya
+// Copyright (c) 2023 rigaya
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -23,33 +23,37 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 //
-// -------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
 
-#pragma once
-#ifndef __RGY_CODEPAGE_H__
-#define __RGY_CODEPAGE_H__
+#ifndef __RGY_BITSTREAM_AAC_H__
+#define __RGY_BITSTREAM_AAC_H__
 
-//日本語環境の一般的なコードページ一覧
-enum : uint32_t {
-    CODE_PAGE_SJIS        = 932, //Shift-JIS
-    CODE_PAGE_JIS         = 50220,
-    CODE_PAGE_EUC_JP      = 51932,
-    CODE_PAGE_UTF8        = 65001,
-    CODE_PAGE_UTF16_LE    = 1200, //WindowsのUnicode WCHAR のコードページ
-    CODE_PAGE_UTF16_BE    = 1201,
-    CODE_PAGE_US_ASCII    = 20127,
-    CODE_PAGE_WEST_EUROPE = 1252,  //厄介な西ヨーロッパ言語
-    CODE_PAGE_UNSET       = 0xffffffff,
+#include <cstdint>
+#include <cstddef>
+
+static const int AAC_HEADER_MIN_SIZE = 7;
+static const uint32_t AAC_BLOCK_SAMPLES = 1024;
+
+struct RGYAACHeader {
+    static const int HEADER_BYTE_SIZE = 7;
+    bool id;
+    bool protection;
+    int profile;     // 00 ... main, 01 ... lc, 10 ... ssr
+    int samplerate;
+    bool private_bit;
+    uint32_t channel;
+    bool original;
+    bool home;
+    bool copyright;
+    bool copyright_start;
+    uint32_t aac_frame_length; // AACヘッダを含む
+    int adts_buffer_fullness;
+    int no_raw_data_blocks_in_frame;
+
+    static bool is_adts_sync(const uint16_t *ptr);
+    static bool is_valid(const uint8_t *buf, const size_t size);
+    int parse(const uint8_t *buf, const size_t size = RGYAACHeader::HEADER_BYTE_SIZE);
+    int sampleRateIdxToRate(const uint32_t idx);
 };
 
-//BOM文字リスト
-static const int MAX_UTF8_CHAR_LENGTH = 6;
-static const uint8_t UTF8_BOM[]     = { 0xEF, 0xBB, 0xBF };
-static const uint8_t UTF16_LE_BOM[] = { 0xFF, 0xFE };
-static const uint8_t UTF16_BE_BOM[] = { 0xFE, 0xFF };
-
-uint32_t jpn_check(const void *str, uint32_t size_in_byte);
-uint32_t get_code_page(const void *str, uint32_t size_in_byte);
-const char *codepage_str(uint32_t codepage);
-
-#endif //__RGY_CODEPAGE_H__
+#endif //__RGY_BITSTREAM_AAC_H__
